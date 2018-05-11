0cffdabab409c8c7823ec21a9365ae616375e3fd
kamal <kkjoshi.it@gmail.com>
2018-05-12 01:03:39 +0530

    kmp

--- src/main/java/lib/string/StringUtils.java
+++ src/main/java/lib/string/StringUtils.java
@@ -1,6 +1,8 @@
 package lib.string;
 
 
+import java.util.Arrays;
+
 public class StringUtils {
 
 	/**
@@ -46,4 +48,49 @@ public class StringUtils {
 
 		return maxLen;
 	}
+
+  public static int kmp(String s, String t) {
+	  int pa[] = prefixArray(t);
+	  int ti = 0;
+	  for (int i = 0; i < s.length(); ) {
+      if (t.charAt(ti) == s.charAt(i)) {
+        ti++;
+        i++;
+      } else {
+        if (ti != 0) ti = pa[ti - 1];
+        else i++;
+      }
+      if (ti >= t.length()) {
+        return i - t.length() + 1;
+      }
+    }
+    return -1;
+  }
+
+  public static int[] prefixArray(String t) {
+    int pArray[] = new int[t.length()];
+	  pArray[0] = 0;
+	  int j = 0;
+    for (int i = 1; i < t.length(); i++) {
+      if (t.charAt(i) == t.charAt(j)) {
+        j++;
+      } else {
+        while (t.charAt(j) != t.charAt(i) && j > 0) {
+          j = pArray[j - 1];
+        }
+        if (j != 0) {
+          j++;
+        }
+      }
+      pArray[i] = j;
+    }
+    return pArray;
+  }
+
+  public static void main(String args[]) {
+    System.out.println(Arrays.toString(prefixArray("acacabacacabacacac")));
+    System.out.println(kmp("acacabacacabacacac", "acacabacacabacacac"));
+    System.out.println(kmp("xacaacacabacacabacacac", "acacabacacabacacac"));
+    System.out.println(kmp("abcabdabc", "abd"));
+  }
 }
